3332c035ca64e7470b0a7b0e73f65cc42cb74a53
ethanarnold <emailtoethan@gmail.com>
2022-01-10 13:21:46 -0800

    2d array practice

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,14 +2,24 @@ public class MyMain {
 
     // Returns the number of odd numbers in mat
     public static int countOdds(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+        int res = 0;
+        for (int[] inner: mat) {
+            for (int x : inner) {
+                if(x % 2 != 0) res++;
+            }
+        }
+        return res;
     }
 
     // Returns the largest value in mat
     public static int findLargest(int[][] mat) {
-        // YOUR CODE HERE
-        return -1;
+        int biggie = mat[0][0];
+        for (int[] inner: mat) {
+            for (int x : inner) {
+                if(x > biggie) biggie = x;
+            }
+        }
+        return biggie;
     }
 
     // Write a method that "left-shifts" each row in a 2D array.
@@ -17,51 +27,92 @@ public class MyMain {
     // shifted to the left by 1, with wraparound (the first value
     // should wrap around to the last entry in the row)
     public static int[][] leftShift(int[][] mat) {
-        // YOUR CODE HERE
-        return null;
+        int temp;
+        for (int row = 0; row < mat.length; row++) {
+            temp = mat[row][0];
+            for (int col = 0; col < mat[0].length-1; col++) {
+                mat[row][col] = mat[row][col+1];
+            }
+            mat[row][mat[0].length-1] = temp;
+        }
+        return mat;
     }
 
     // Creates a new array list of size arraySize x arraySize
     // where each value is each to the sum of its row and col indexes
     public static int[][] arrayBuilder(int arraySize) {
-        // YOUR CODE HERE
-        return null;
+        int[][] mat = new int[arraySize][arraySize];
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                mat[row][col] = row + col;
+            }
+        }
+        return mat;
     }
-
-
-
-
-
-
-
-
-
 
     // Methods for the homework:
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        double sum = 0.0;
+        int numValues = 0;
+        for (double[] inner : mat) {
+            for (double x : inner) {
+                numValues++;
+                sum += x;
+            }
+        }
+        return sum/numValues;
     }
 
     // Returns the median of the 2D array mat
     // Remember that the array is sorted!
     public static double median(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        int row = mat.length;
+        int col = mat[0].length;
+        if (row % 2 != 0 && col % 2 != 0) {
+            return mat[row / 2][col / 2];
+        }
+        if (row % 2 != 0) {
+            return (mat[row/2][col/2-1] + mat[row/2][col/2])/2;
+        }
+        return (mat[row/2-1][col-1] + mat[row/2][0]) / 2;
     }
 
 
     // Returns the mode of the 2D array mat
     // Remember that the array is sorted!
     public static double mode(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        double mode = mat[0][0];
+        int count = count(mode, mat);
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if(count(mat[row][col], mat) > count) {
+                    mode = mat[row][col];
+                    count = count(mode, mat);
+                }
+            }
+        }
+        return mode;
+    }
+
+    public static int count(double num, double[][] mat) {
+        int count = 0;
+        for (double[] inner : mat) {
+            for (double x : inner) {
+                if(Math.abs(x-num) < 0.00001) count++;
+            }
+        }
+        return count;
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        double[][] testMat = {
+                {1,  2,  2,  2,  6},
+                {7,  9,  10, 11, 11},
+                {11, 11, 17, 18, 20} };
+
+        System.out.println(count(6, testMat));
     }
 }
